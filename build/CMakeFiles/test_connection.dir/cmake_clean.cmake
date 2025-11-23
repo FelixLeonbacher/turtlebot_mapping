570d9a1dc6a9a9b02c2e70596dcdfdf72198baea
Felix Mobile <padavanilie@gmail.com>
2025-11-23 16:02:21 +0100

    joah, erster scan ist schon mal gut

---
+++ build/CMakeFiles/test_connection.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_connection.dir/src/test_connection.cpp.obj"
+  "CMakeFiles/test_connection.dir/src/test_connection.cpp.obj.d"
+  "libtest_connection.dll.a"
+  "test_connection.exe"
+  "test_connection.exe.manifest"
+  "test_connection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_connection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/turtlebot_core.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/padav/Desktop/turtlebot_mapping/src/connection/connection.cpp" "CMakeFiles/turtlebot_core.dir/src/connection/connection.cpp.obj" "gcc" "CMakeFiles/turtlebot_core.dir/src/connection/connection.cpp.obj.d"
+  "C:/Users/padav/Desktop/turtlebot_mapping/src/core/parser.cpp" "CMakeFiles/turtlebot_core.dir/src/core/parser.cpp.obj" "gcc" "CMakeFiles/turtlebot_core.dir/src/core/parser.cpp.obj.d"
+  "C:/Users/padav/Desktop/turtlebot_mapping/src/export/export.cpp" "CMakeFiles/turtlebot_core.dir/src/export/export.cpp.obj" "gcc" "CMakeFiles/turtlebot_core.dir/src/export/export.cpp.obj.d"
+  "C:/Users/padav/Desktop/turtlebot_mapping/src/main.cpp" "CMakeFiles/turtlebot_core.dir/src/main.cpp.obj" "gcc" "CMakeFiles/turtlebot_core.dir/src/main.cpp.obj.d"
+  "C:/Users/padav/Desktop/turtlebot_mapping/src/mapping/mapping.cpp" "CMakeFiles/turtlebot_core.dir/src/mapping/mapping.cpp.obj" "gcc" "CMakeFiles/turtlebot_core.dir/src/mapping/mapping.cpp.obj.d"
+  "C:/Users/padav/Desktop/turtlebot_mapping/src/pathing/lincontrol.cpp" "CMakeFiles/turtlebot_core.dir/src/pathing/lincontrol.cpp.obj" "gcc" "CMakeFiles/turtlebot_core.dir/src/pathing/lincontrol.cpp.obj.d"
+  "C:/Users/padav/Desktop/turtlebot_mapping/src/test_connection.cpp" "CMakeFiles/turtlebot_core.dir/src/test_connection.cpp.obj" "gcc" "CMakeFiles/turtlebot_core.dir/src/test_connection.cpp.obj.d"
+  "C:/Users/padav/Desktop/turtlebot_mapping/src/test_mapping_1.cpp" "CMakeFiles/turtlebot_core.dir/src/test_mapping_1.cpp.obj" "gcc" "CMakeFiles/turtlebot_core.dir/src/test_mapping_1.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
